73031192ccc23d5f4c6ce39789d2aececccec8d3
ghdej <ghdej@DESKTOP-M1TQRAQ>
2020-08-25 10:20:40 +0900

    Spring05 트랜잭션 관리 작성중2

--- Spring05_Final/src/main/java/com/gura/spring05/exception/NoDeliveryException.java
+++ Spring05_Final/src/main/java/com/gura/spring05/exception/NoDeliveryException.java
@@ -10,7 +10,7 @@ public class NoDeliveryException extends DataAccessException{
 	 */
 	public NoDeliveryException(String msg) {
 		super(msg);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
